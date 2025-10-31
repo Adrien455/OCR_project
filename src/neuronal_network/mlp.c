975dc9added2bb2831f447d8a78c478f89b9af22
Adrien455 <adrien.meyssonnier@icloud.com>
2025-10-31 15:47:36 +0100

    update_mlp

--- src/neuronal_network/mlp.c
+++ src/neuronal_network/mlp.c
@@ -9,7 +9,7 @@ MLP *mlp_create(int in, int hid)
     MLP *m = (MLP *)calloc(1, sizeof(MLP));
     m -> in = in; m -> hid = hid;
     m -> W1 = (double *)malloc(sizeof(double) * hid * in);
-    m -> b1 = (double  *)calloc(hid, sizeof(double));
+    m -> b1 = (double *)calloc(hid, sizeof(double));
     m -> W2 = (double *)malloc(sizeof(double) * hid);
     m -> b2 = 0.0;
     for (int i = 0; i < hid * in; ++i) m -> W1[i] = ((rand()%2000) / 1000.0 - 1.0) * 0.1;
@@ -39,26 +39,30 @@ double mlp_forward(const MLP *m, const double *x, double *h)
 void mlp_backward(MLP *m, const double *x, const double *h, double y, double t, double lr) 
 {
     double dldy = (y - t); // derivative for BCE with sigmoid
+
+    // update output weights
     for (int j = 0; j < m -> hid; ++j) 
     {
         m -> W2[j] -= lr * dldy * h[j];
     }
-
     m -> b2 -= lr * dldy;
-    double dh[m -> hid];
 
+    // backpropagate to hidden
+    double dh[m -> hid];
     for (int j = 0; j < m -> hid; ++j) 
     {
         double d = m -> W2[j] * dldy;
         dh[j] = d * h[j] * (1.0 - h[j]);
     }
 
+    // update input -> hidden weights
     for (int j = 0; j < m -> hid; ++j) 
     {
         for (int i = 0; i < m -> in; ++i)
         {
             m -> W1[j * m -> in + i] -= lr * dh[j] * x[i];
-            m -> b1[j] -= lr * dh[j];
         }
+
+        m -> b1[j] -= lr * dh[j];
     }
 }
